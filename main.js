c52cf39d570463795f937db58e8e74f0a84924be
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-18 20:58:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Moralis init code */
+const serverUrl = "SERVER_ID";
+const appId = "APP_ID";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: "Log in using Moralis Test",
+    })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
